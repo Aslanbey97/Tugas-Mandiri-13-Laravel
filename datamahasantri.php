e6bc39d9b24043223ceb4538d4f7d16ec86ccba3
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2023-04-15 13:35:17 +0700

    Add files via upload

---
+++ datamahasantri.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class datamahasantri extends Model
+{
+    use HasFactory;
+    // tambahkan kode untuk mapping ke table datamahasantri
+    protected $table = 'datamahasantri';
+}
+ 
